99cc5fa0e71041402d72a6e44313d63231c2715b
emilio-code7 <253608500+emilio-code7@users.noreply.github.com>
2026-07-10 14:30:59 +0200

    feat: add procesion-service project skeleton

---
+++ services/procesion-service/src/main/java/com/repertorio/procesion/ProcesionServiceApplication.java
@@ -0,0 +1,20 @@
+package com.repertorio.procesion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
+@EnableMethodSecurity
+@EnableScheduling
+public class ProcesionServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProcesionServiceApplication.class, args);
+    }
+}
